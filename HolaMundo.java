66c58e6b503dcb38ab0c5adab4bbc25204567cd7
Mily <milycarrasco85@gmail.com>
2025-09-21 21:53:21 -0500

    Cambio en main

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,6 +1,6 @@
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola es mi segundo commit en Java");
-        System.out.println("Aprendiendo ramas");
+        System.out.println("Soy la version main");
     }
 }
